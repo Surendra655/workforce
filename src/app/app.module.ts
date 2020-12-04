3ef1ed3b0dd07029dd63b3138523c2b77f7ed151
Surendra Kumar <surendra@apptrinity>
2020-12-04 18:59:48 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeAddComponent
   ],
   imports: [
+    ReactiveFormsModule,FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
